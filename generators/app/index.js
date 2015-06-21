10be3054e217e211b1b1df46b3145899abfaafca
Sota Yamashita <sota.yamashita@gmail.com>
2015-06-22 00:41:00 +0900

    Add: package.json: standard library

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,30 +1,30 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+'use strict'
+var yeoman = require('yeoman-generator')
+var chalk = require('chalk')
+var yosay = require('yosay')
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
-    var done = this.async();
+    var done = this.async()
 
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the mathematical ' + chalk.red('PsgTheme') + ' generator!'
-    ));
+    ))
 
     var prompts = [{
       type: 'confirm',
       name: 'someOption',
       message: 'Would you like to enable this option?',
       default: true
-    }];
+    }]
 
     this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+      this.props = props
+      // To access props later use this.props.someOption
 
-      done();
-    }.bind(this));
+      done()
+    }.bind(this))
   },
 
   writing: {
@@ -32,26 +32,26 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copy(
         this.templatePath('_package.json'),
         this.destinationPath('package.json')
-      );
+      )
       this.fs.copy(
         this.templatePath('_bower.json'),
         this.destinationPath('bower.json')
-      );
+      )
     },
 
     projectfiles: function () {
       this.fs.copy(
         this.templatePath('editorconfig'),
         this.destinationPath('.editorconfig')
-      );
+      )
       this.fs.copy(
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
-      );
+      )
     }
   },
 
   install: function () {
-    this.installDependencies();
+    this.installDependencies()
   }
-});
+})
